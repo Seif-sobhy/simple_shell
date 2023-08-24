82094bb02a58bcc69868523bc3e67d7bfeb7369a
seif sobhy <seifsobhy5@gmail.com>
2023-08-24 15:47:24 +0300

    A

--- shell.h
+++ shell.h
@@ -1,17 +1,17 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
+#ifndef _SHELL_H
+#define _SHELL_H
 
-/**###### environ var ######*/
+/*###### environ var ######*/
 
 extern char **environ;
 
-/**##### MACROS ######*/
+/*##### MACROS ######*/
 
 #define BUFSIZE 1024
 #define DELIM " \t\r\n\a"
 #define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
 
-/**###### LIBS USED ######*/
+/*###### LIBS USED ######*/
 
 #include <stdio.h>
 #include <unistd.h>
@@ -29,7 +29,7 @@ extern char **environ;
 
 
 
-/**###### STRING FUNCTION ######*/
+/*###### STRING FUNCTION ######*/
 
 char *_stringtoken(char *str, const char *tok);
 unsigned int check_diff(char c, const char *str);
@@ -49,39 +49,39 @@ char *_stchr(char *s, char c);
 int _stringcmp(const char *s1, const char *s2, size_t n);
 char *_stdp(char *str);
 
-/**###### MEMORIE  MANGMENT ####*/
+/*###### MEMORIE  MANGMENT ####*/
 
-void free_environment(char **env);
-void *fill_array(void *a, int el, unsigned int len);
-char *copy_memory(char *dest, char *src, unsigned int n);
-void *allocate_memory(unsigned int size);
-void *_mem_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void  free_all_strings(char **input, char *line);
+void free_env(char **env);
+void *fill_an_array(void *a, int el, unsigned int len);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_calloc(unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void  free_all(char **input, char *line);
 
-/**###### INPUT Function ######*/
+/*###### INPUT Function ######*/
 
-void display_shell_prompt(void);
+void prompt(void);
 void signal_to_handel(int sig);
 char *_getline(void);
 
-/** ###### Command parser and extractor ###*/
+/* ###### Command parser and extractor ###*/
 
-int search_path(char **line);
-char *get_env(char *name);
+int path_cmd(char **line);
+char *_getenv(char *name);
 char **p_cmd(char *cmd);
 int handle_builtin(char **cmd, int er);
 void read_file(char *filename, char **argv);
-char *build_path(char *token, char *value);
-int check_builtin(char **cmd);
+char *build(char *token, char *value);
+int is_builtin(char **cmd);
 void creat_envi(char **envi);
 int check_cmd(char **tokens, char *line, int count, char **argv);
 void treat_file(char *line, int counter, FILE *fd, char **argv);
 void exit_bul_for_file(char **cmd, char *line, FILE *fd);
 
-/** ####BUL FUNC #####*/
+/* ####BUL FUNC #####*/
 
 void hashtag_handle(char *buff);
-int custom_history(char *input);
+int history(char *input);
 int history_display(char **cmd, int er);
 int ds_envir(char **cmd, int er);
 int ch_directory(char **cmd, int er);
@@ -90,11 +90,11 @@ int echo_bul(char **cmd, int er);
 void  ex_bl(char **cmd, char *input, char **argv, int c);
 int pr_echo(char **cmd);
 
-/** ####error handle and Printer ####*/
+/* ####error handle and Printer ####*/
 void print_no(unsigned int n);
 void print_no_in(int n);
 void print_error(char *line, int c, char **argv);
-void custom_error_print(char **argv, int c, char **cmd);
+void _prerror(char **argv, int c, char **cmd);
 
 
 /**
